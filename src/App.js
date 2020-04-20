1c1f9a557ae48d744177b5ac3fc1e79e5af695e9
NhanMai <mvtnhan@gmail.cpm>
2020-04-20 08:51:26 +0700

    create components and scss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./scss/reset.scss";
+import "./App.scss";
+
+import TodoHeader from "./components/todo-header.js";
+import TodoItem from "./components/todo-item.js";
+import TodoItem2 from "./components/todo-item.js";
+import TodoFooter from "./components/todo-footer.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      <h1 className="title">Todos</h1>
+      {TodoHeader()}
+      {TodoItem()}
+      {TodoItem2()}
+      {TodoFooter()}
     </div>
   );
 }
