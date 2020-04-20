d03a1b703916e3668a429f53e663e1a4264ad209
NhanMai <mvtnhan@gmail.cpm>
2020-04-20 08:51:26 +0700

    create PR

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      <div className="header">
+        <h1>Todos</h1>
+        {/* <button className="btn-todo" /> */}
+        <input
+          className="new-todo"
+          type="text"
+          placeholder="What needs to be done?"
+        />
+      </div>
+
+      <div className="item">
+        <input className="toggle-all" type="checkbox" />
+        <label for="toggle-all" />
+        <ul className="todo-list">
+          <li>
+            <div className="view">
+              <input className="toggle" type="checkbox"></input>
+              <label>Todo one</label>
+              <button className="delete"></button>
+            </div>
+            <input className="edit" value="Item1"></input>
+          </li>
+          <li>
+            <div className="view">
+              <input className="toggle" type="checkbox"></input>
+              <label>Todo two</label>
+              <button className="delete"></button>
+            </div>
+            <input className="edit" value="Item1"></input>
+          </li>
+          <li>
+            <div className="view">
+              <input className="toggle" type="checkbox"></input>
+              <label>Todo three</label>
+              <button className="delete"></button>
+            </div>
+            <input className="edit" value="Item1"></input>
+          </li>
+        </ul>
+      </div>
+
+      <div className="footer">
+        <span className="todo-count">
+          <strong>1</strong>
+          <span> </span>
+          <span>item</span>
+          <span> left</span>
+        </span>
+        <ul className="filters">
+          <li>
+            <a href="#/">All</a>
+          </li>
+          <span> </span>
+          <li>
+            <a href="#/active">Active</a>
+          </li>
+          <span> </span>
+          <li>
+            <a href="#/completed">Complected</a>
+          </li>
+          <span> </span>
+        </ul>
+        <button className="clear-completed">Clear completed</button>
+      </div>
     </div>
   );
 }
