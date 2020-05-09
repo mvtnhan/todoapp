46193f0e97f3d7d9930cda6834796ece331dbdf9
NhanMai <mvtnhan@gmail.cpm>
2020-05-09 19:06:45 +0700

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-Implement a todo app using ReactJS
---
+++ precache-manifest.36fd9ede7c69a919ff7c120053273c6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b99d545bc06828463b25dc5b1a1b773",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6c4fb61a83eab3cc755",
+    "url": "/static/css/main.db64a20a.chunk.css"
+  },
+  {
+    "revision": "e5f77d2cead6656a0041",
+    "url": "/static/js/2.6c94263a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c94263a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6c4fb61a83eab3cc755",
+    "url": "/static/js/main.8c91c5c8.chunk.js"
+  },
+  {
+    "revision": "8131cb44685b7ca08ba2",
+    "url": "/static/js/runtime-main.0949d8ab.js"
+  },
+  {
+    "revision": "6ef2b84790dbffc19d0e8bbfafbc861e",
+    "url": "/static/media/checkbox-todo-active.6ef2b847.svg"
+  },
+  {
+    "revision": "40e0f797929ce7b185ed654ae67b7f13",
+    "url": "/static/media/checkbox-todo.40e0f797.svg"
+  }
+]);
