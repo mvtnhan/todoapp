86484929de8405bbf661abf6e2effc768a6a8a3c
NhanMai <mvtnhan@gmail.cpm>
2020-06-05 17:13:24 +0700

    Updates

---
+++ precache-manifest.6c8b8a313665a362680c26bcdd75e848.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f494946414288de4d3850cd3fa3533",
+    "url": "./index.html"
+  },
+  {
+    "revision": "08051555f08673ac02d8",
+    "url": "./static/css/main.bb3efbeb.chunk.css"
+  },
+  {
+    "revision": "8e382df89ee943654c9b",
+    "url": "./static/js/2.376b55b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.376b55b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08051555f08673ac02d8",
+    "url": "./static/js/main.203d81fb.chunk.js"
+  },
+  {
+    "revision": "ff3369f54bee952da010",
+    "url": "./static/js/runtime-main.7a553f36.js"
+  },
+  {
+    "revision": "6ef2b84790dbffc19d0e8bbfafbc861e",
+    "url": "./static/media/checkbox-todo-active.6ef2b847.svg"
+  },
+  {
+    "revision": "40e0f797929ce7b185ed654ae67b7f13",
+    "url": "./static/media/checkbox-todo.40e0f797.svg"
+  }
+]);
